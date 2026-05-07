29c90b71acbf1c09ce4ce10ad53b3db1a50a4d2b
maksimcizov <maksimcizov@gmail.com>
2026-05-07 14:21:20 +0300

    Added a script to test the MongoDB connection

--- test_connection.py
+++ test_connection.py
@@ -1,1 +1,10 @@
+from pymongo import MongoClient
 
+MONGO_URI = "mongodb://localhost:27017/"
+
+client = MongoClient(MONGO_URI)
+db = client["ais"]
+collection = db["test"]
+
+collection.insert_one({"msg": "hello mongo"})
+print(collection.find_one({"msg": "hello mongo"}))
